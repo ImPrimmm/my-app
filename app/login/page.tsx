dbfb078706852f18645cae34b53893bf6f7967c3
ImPrimmm <primaxrpla@gmail.com>
2025-08-20 14:05:46 +0700

    membuat ui login

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+import { LoginGoogleButton } from '@/components/button-google'
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <div>Halaman LOgin</div>
+            <LoginGoogleButton />
+        </>
+    )
+}
+
+export default page
